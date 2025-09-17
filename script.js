ec4ab3d70d95f079f61ea0d2ee01e47638961ced
rubelart881-commits <rubelart881@gmail.com>
2025-09-17 14:13:55 +0600

    script.js

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ import "firebase/database"
 
 // Firebase Configuration - Replace with your own Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyA6Qcog97mvfc_RysaH420wdIqNweGgHg8",
-  authDomain: "tgp-group-work.firebaseapp.com",
-  databaseURL: "https://tgp-group-work-default-rtdb.firebaseio.com", 
-  projectId: "tgp-group-work",
-  storageBucket: "tgp-group-work.appspot.com",
-  messagingSenderId: "254968365131",
-  appId: "1:254968365131:web:bedae8eaf2c2a71959cdce",
-  measurementId: "G-8K6NH5KGZF"
+ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
